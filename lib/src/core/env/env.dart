9165240742ac024cee463736c12f1402355bae73
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-06-18 23:41:44 -0400

    feat: handle env based on flavor

--- lib/src/core/env/env.dart
+++ lib/src/core/env/env.dart
@@ -5,11 +5,11 @@ part 'env.g.dart';
 @Envied(path: '.env.dev', useConstantCase: true)
 abstract class DevEnv {
   @EnviedField()
-  static const String baseApiUrl = String.fromEnvironment('BASE_API_URL');
+  static const String baseApiUrl = _DevEnv.baseApiUrl;
 }
 
 @Envied(path: '.env.prod', useConstantCase: true)
 abstract class ProdEnv {
   @EnviedField()
-  static const String baseApiUrl = String.fromEnvironment('BASE_API_URL');
+  static const String baseApiUrl = _ProdEnv.baseApiUrl;
 }
