5b5519b67a2ce7c7cad126a01cbc622631312a6e
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-07-06 13:38:19 -0400

    feature: create  prediction models, provider and api

--- lib/src/di.dart
+++ lib/src/di.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 
+import 'package:ai_animals_lottery/src/features/predictions/predictions_provider.dart';
 import 'package:ai_animals_lottery/src/features/results/results_provider.dart';
 
 final di = GetIt.instance;
@@ -7,4 +8,5 @@ final di = GetIt.instance;
 /// Initializes the dependencies.
 void init() {
   di.registerLazySingleton(() => ResultsProvider());
+  di.registerLazySingleton(() => PredictionsProvider());
 }
---
+++ lib/src/features/predictions/models/prediction.dart
@@ -0,0 +1,72 @@
+class Prediction {
+  /// The date of the prediction.
+  final DateTime date;
+
+  /// The lottery house of the prediction.
+  final String lotteryHouse;
+
+  /// The animal of the prediction.
+  final String animal;
+
+  /// Wether the prediction is a winner.
+  final bool isWinner;
+
+  /// The number of successful predictions in the last 7 days.
+  final int lastSevenDays;
+
+  /// The number of successful predictions in the last 15 days.
+  final int lastFifteenDays;
+
+  /// The number of successful predictions in the last 30 days.
+  final int lastThirtyDays;
+
+  const Prediction({
+    required this.date,
+    required this.lotteryHouse,
+    required this.animal,
+    required this.isWinner,
+    required this.lastSevenDays,
+    required this.lastFifteenDays,
+    required this.lastThirtyDays,
+  });
+
+  /// Creates a [Prediction] from a JSON object.
+  factory Prediction.fromJson(Map<String, dynamic> json) {
+    return Prediction(
+      date: DateTime.parse(json['fecha']),
+      lotteryHouse: json['desc_loteria'],
+      animal: json['desc_animal'],
+      isWinner: json['salio'] != null,
+      lastSevenDays: int.parse(json['d7']),
+      lastFifteenDays: int.parse(json['d15']),
+      lastThirtyDays: int.parse(json['d30']),
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Prediction &&
+        other.date == date &&
+        other.lotteryHouse == lotteryHouse &&
+        other.animal == animal &&
+        other.isWinner == isWinner &&
+        other.lastSevenDays == lastSevenDays &&
+        other.lastFifteenDays == lastFifteenDays &&
+        other.lastThirtyDays == lastThirtyDays;
+  }
+
+  @override
+  int get hashCode {
+    return Object.hash(
+      date,
+      lotteryHouse,
+      animal,
+      isWinner,
+      lastSevenDays,
+      lastFifteenDays,
+      lastThirtyDays,
+    );
+  }
+}
